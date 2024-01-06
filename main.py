456119b13e3b238e55469208927dc62175ca6dfe
Mateus Lucas <mateus.lucas97@yahoo.com.br>
2024-01-05 21:56:07 -0300

    versao 1.5

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+
 import socket
 from threading import Thread
+import time
 
 from tkinter import *
+
+IP_SERVIDOR = "192.168.0.51"
 
 class Application():
     def __init__(self, root):
@@ -9,7 +13,7 @@ class Application():
         self.janela()
         self.tela()
         self.botao()
-        self.cor_padrao = "white"
+        self.cor_padrao = "black"
         self.cor_alerta = "red"
         self.servidor = True
 
@@ -27,12 +31,6 @@ class Application():
         self.tela_consultorio.place(x=5, y=5, width=100, height=85)
         self.tela_consultorio.config (font=("Arial", 13))
 
-        #cria o botao "Stop"
-    def botao(self):
-        self.botao_stop = Button(self.root, text="Parar")
-        self.botao_stop.config(font=("Arial", 20))
-        self.botao_stop.place(x=5, y=100, width=100, height=50)
-
         #define o tamanho da tela para o padrao full HD 1920 x 1080
         largura = 800
         altura = 800
@@ -44,12 +42,19 @@ class Application():
 
         self.root.resizable(False, False)
 
+        #cria o botao "Stop"
+    def botao(self):
+        self.botao_stop = Button(self.root, text="Parar")
+        self.botao_stop.config(font=("Arial", 20))
+        self.botao_stop.place(x=5, y=100, width=100, height=50)
+
+
     def start_server(self):
         #Cria um socket TCP/IP
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         #Associa o socket a uma porta
-        sock.bind(("10.16.90.122", 8080))
+        sock.bind((IP_SERVIDOR, 8080))
 
         #coloca o socket em modo de escuta
         sock.listen(1)
@@ -64,8 +69,12 @@ class Application():
 
                 #verifica o tipo de requisicao
                 if request == "alerta":
+
+                    for i in range(10):
                     #altera a cor da tela para vermelho
-                    self.tela_consultorio.config(foreground=self.cor_alerta)
+                        self.tela_consultorio.config(foreground=self.cor_alerta)
+                        time.sleep(0.1)
+                        self.tela_consultorio.config(foreground=self.cor_padrao)
                 else:
                     #altera a cor da tela para a cor padrao
                     self.tela_consultorio.config(foreground=self.cor_padrao)
@@ -80,9 +89,18 @@ class Application():
 
     def parar_servidor(self):
         self.servidor = False
-        self.server_thread.join()
-        self.tela_consultorio.config(foreground=self.cor_padrao)
-                
+
+        # Verifica a cor da tela
+        cor_tela = self.tela_consultorio.cget("foreground")
+
+        # Altera a cor da tela para a cor padrão
+        if cor_tela == self.cor_alerta:
+            self.tela_consultorio.config(foreground=self.cor_padrao)
+
+        # Continua o servidor em execução
+        self.server_thread.start()
+
+    
 
     def receber_requisicao(self, evento):
         if evento.data == "alerta":
